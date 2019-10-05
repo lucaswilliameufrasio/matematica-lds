5e4d40c75ec798a760c8bc32183e5367d5365648
Lucas William <lucaswilliameufrasio@gmail.com>
2019-10-05 07:59:10 -0300

    Adição de bibliotecas e funções

--- Frontend/src/pages/home/home.ts
+++ Frontend/src/pages/home/home.ts
@@ -4,6 +4,9 @@ import { SocialSharing } from '@ionic-native/social-sharing';
 import { CalculadoraPage } from '../calculadora/calculadora';
 import { MenuPage } from '../menu/menu';
 import { LoginPage } from '../login/login';
+import { Storage, IonicStorageModule } from '@ionic/storage';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -22,6 +25,8 @@ export class HomePage {
     public navCtrl: NavController,
     private socialSharing: SocialSharing,
     public nav: NavController,
+    public http: HttpClient,
+    private storage: Storage,
   ) {
     this.pushCalculator = CalculadoraPage;
     this.pushMenu = MenuPage;
@@ -30,6 +35,18 @@ export class HomePage {
     this.params_multiplication = { id: 3 };
     this.params_Division = { id: 4 };
     this.params_Exponentiation = { id: 5 };
+  }
+  ionViewDidLoad() {
+    this.storage.get('access_token').then(val => {
+      if (val != null) {
+        console.log("Token armazenado : ", val)
+      }
+      else {
+        console.log("Valor nulo ou indefinido:", val)
+      }
+    }, err => {
+      console.log("Erro no Storage : ", err)
+    })
   }
 
   openCalculadora() {
@@ -44,7 +61,44 @@ export class HomePage {
     });
   }
 
-  goToLogin() {
+  logout() {
+
+    alert("Saiu com sucesso.")
+    this.storage.remove('access_token');
     this.nav.push(LoginPage);
+
+    // let api = 'http://127.0.0.1:8001';
+
+    // this.storage.get('access_token').then((token) => {
+    //   if (token != null) {
+    //     let headers = new HttpHeaders({});
+
+    //     console.log(token);
+    //     headers = headers.set('Authorization', 'Bearer ' + token);
+    //     headers = headers.set('Accept', 'application/json');
+    //     headers = headers.set('Access-Control-Allow-Origin', '*');
+    //     console.log(headers);
+
+
+    //     this.http.post(api + '/auth/logout', {
+    //       headers: new HttpHeaders().set('Authorization', 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMVwvYXV0aFwvbG9naW4iLCJpYXQiOjE1NzAyNzE0NzMsImV4cCI6MTU3MDI3NTA3MywibmJmIjoxNTcwMjcxNDczLCJqdGkiOiJhc0p4MEFmb1kxRDFRVWIzIiwic3ViIjozLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.CfBCbYUEakdKnportFU5o9GTz-WdYWQdri-xnuZBrcU'),
+    //     })
+    //       .subscribe(res => {
+    //         console.log(res);
+    //         console.log(headers)
+    //         if (res['success']) {
+    //           alert("Saiu com sucesso.")
+    //           this.storage.remove('access_token');
+    //           this.nav.push(LoginPage);
+    //         } else {
+    //           alert(res)
+    //         }
+    //       }, (error) => {
+    //         console.log(error);
+    //       });
+    //     }
+    //   }
+    // )
+    
   }
 }
