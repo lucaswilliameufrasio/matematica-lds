aea4e10f20130ef6515313765efbf36dbb22bef5
Lucas William <lucaswilliameufrasio@gmail.com>
2019-10-05 04:38:41 -0300

    Correção de localização da model User

--- Backend/app/Http/Controllers/AuthController.php
+++ Backend/app/Http/Controllers/AuthController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\User;
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
