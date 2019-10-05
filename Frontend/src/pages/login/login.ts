106fb6be64bc2c7cab5a72806f894688eaceeb6f
Lucas William <lucaswilliameufrasio@gmail.com>
2019-10-05 07:59:42 -0300

    Tornando login funcional

--- Frontend/src/pages/login/login.ts
+++ Frontend/src/pages/login/login.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
 import { CadastroPage } from '../cadastro/cadastro';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Storage, IonicStorageModule } from '@ionic/storage';
 
 @IonicPage()
 @Component({
@@ -10,11 +12,16 @@ import { CadastroPage } from '../cadastro/cadastro';
 })
 export class LoginPage {
 
+  email: string = '';
+  senha: string = '';
+
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public navParams: NavParams,
     public nav: NavController,
-    ) {
+    public http: HttpClient,
+    private storage: Storage,
+  ) {
   }
 
   ionViewDidLoad() {
@@ -22,7 +29,29 @@ export class LoginPage {
   }
 
   goToHome() {
-    this.nav.setRoot(HomePage);
+    alert('Email: ' +this.email);
+
+    let api = 'http://127.0.0.1:8001';
+    let dados = JSON.stringify({ email: this.email, password: this.senha });
+
+    this.http.post(api + '/auth/login', dados, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }),
+    })
+      .subscribe(res => {
+        console.log(res);
+        if (res['success']) {
+          alert("Logado com sucesso.")
+          this.storage.set('access_token', res['access_token']);
+          this.nav.setRoot(HomePage);
+        } else {
+          alert("O email ou senha parecem estar errados.")
+        }
+      }, (error) => {
+        console.log(error);
+      });
   }
 
   goToCadastro() {
