78f8d7ade9aa727fc53c1185d66728ff8adf30f4
Lucas William <lucaswilliameufrasio@gmail.com>
2019-11-16 01:00:19 -0300

    Criação da migration de ranking

---
+++ Backend/database/migrations/2019_11_15_181300_create_rankings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRankingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rankings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('score');
+            $table->string('time');
+            $table->unsignedBigInteger('operation_type');
+            $table->foreign('operation_type')->references('id')->on('math_operations');
+            $table->unsignedBigInteger('users_id');
+            $table->foreign('users_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rankings');
+    }
+}
