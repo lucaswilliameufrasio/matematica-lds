1de57e29340793f116f16c6b9aa29bfe432ee5ba
Lucas William <lucaswilliameufrasio@gmail.com>
2019-12-06 15:01:21 -0300

    Adição de uma query estruturada do ranking e posição do usuário autenticado

--- Backend/app/Http/Controllers/Rankings/RankingsController.php
+++ Backend/app/Http/Controllers/Rankings/RankingsController.php
@@ -5,21 +5,29 @@ namespace App\Http\Controllers\Rankings;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Ranking;
+use Illuminate\Support\Facades\DB;
 
 class RankingsController extends Controller
 {
     public function showUserRanking($operation)
     {
-        $ranking = Ranking::where("mathoperation_id", $operation)
-            ->get();
+        $ranking = DB::table('rankings')
+        ->join('users', 'rankings.users_id', '=', 'users.id')
+        ->join('matches', 'rankings.match_id', '=', 'matches.id')
+        ->select('users.id as userid', 'users.name as name' ,'matches.score as score', 'matches.time as time')
+        ->where('rankings.mathoperation_id', $operation)
+        ->orderBy('matches.score', 'DESC')
+        ->orderBy('matches.time', 'ASC')
+        ->get();
 
-        $yourPos = Ranking::where("mathoperation_id", $operation)
+        $yourPos = Ranking::select('users_id as myid')->where("mathoperation_id", $operation)
             ->where('users_id', auth()->user()->id)
             ->get();
 
         return response()->json(
             [
                 'data' => $ranking,
+                'myposition' => $yourPos,
             ],
             200,
             ['Content-type' => 'application/json; charset=utf-8'],
