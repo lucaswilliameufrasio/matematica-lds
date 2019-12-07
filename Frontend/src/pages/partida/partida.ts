bbd677ec16aa5f9c720cd30fe8b5a2cc36b08eb9
Marcos Henrique de Sousa Santos <henriquesantos.uefa@gmail.com>
2019-12-07 00:10:04 -0300

    adicionando ranking

--- Frontend/src/pages/partida/partida.ts
+++ Frontend/src/pages/partida/partida.ts
@@ -12,13 +12,15 @@ import { RankingPage } from '../ranking/ranking';
 export class PartidaPage {
 
   Selected_Operation: any;
+  ranking: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private rankingService: RankingService) {
     this.Selected_Operation = this.navParams.data;
     console.log("id operação: ", this.Selected_Operation.id);
   }
 
   ionViewDidLoad() {
+    this.getRanking();
     console.log('ionViewDidLoad PartidaPage');
   }
 
@@ -28,6 +30,14 @@ export class PartidaPage {
 
   goToRaking(){
     this.navCtrl.push(RankingPage, {Selected_Operation: this.Selected_Operation})
+  }
+
+  getRanking() {
+    let rankingTemp = [];
+    this.rankingService.ranking(this.Selected_Operation.id)
+      .subscribe(res => {
+        this.ranking = res;
+      });
   }
 
 }
--- Frontend/src/pages/ranking/ranking.ts
+++ Frontend/src/pages/ranking/ranking.ts
@@ -10,8 +10,9 @@ import { RankingService } from '../../services/ranking.service';
 export class RankingPage {
 
   ranking: Array<any> = [];
+
   Selected_Operation: any;
-  
+
   constructor(public navCtrl: NavController, public navParams: NavParams, private rankingService: RankingService) {
     this.Selected_Operation = this.navParams.data.Selected_Operation;
   }
@@ -21,11 +22,20 @@ export class RankingPage {
     this.getRanking()
   }
 
-  getRanking(){
+  getRanking() {
+    let rankingTemp = [];
     this.rankingService.ranking(this.Selected_Operation.id)
-    .subscribe(res => {
-      this.ranking = res
-      console.log("Ranking:::", res);
+      .subscribe(res => {
+        this.ranking = res.data
+        this.ranking.sort((a, b) => (a.score < b.score) ? -1 : 1);
+        console.log("Ranking:::", this.ranking);
+      });
+  }
+
+  public sortByKey(array, key) {
+    return array.sort(function (a, b) {
+      var x = a[key]; var y = b[key];
+      return ((x < y) ? -1 : ((x > y) ? 0 : 1));
     });
   }
 }
