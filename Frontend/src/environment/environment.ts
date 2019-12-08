2682940497bb5d443239489c9406929e87acea55
Lucas William <lucaswilliameufrasio@gmail.com>
2019-12-08 17:00:07 -0300

    Atualização dos comentários do arquivo environment

--- Frontend/src/environment/environment.ts
+++ Frontend/src/environment/environment.ts
@@ -1,7 +1,7 @@
-//Adicone os links aqui
+//Adicione aqui o endereço o qual a API está rodando
 export const environment = {
-  //server: 'http://127.0.0.1:8001' // Link da API
+  //server: 'http://127.0.0.1:8001'
   //server: 'http://backend.test.com/'
-  server: 'https://labds.herokuapp.com'
-  //server: 'http://127.0.0.1:8000'
+  server: 'https://labds.herokuapp.com' // Link da API em produção
+  //server: 'http://127.0.0.1:8000' // Link da API local
 };
